05f9b40c902a0e5acbc07f43160b3d0eeded0c2c
preethi <preethimandipalli99@gmail.com>
2021-04-12 22:10:10 +0530

    refactored equals method such that it can compare values of different units

--- src/main/java/utility/Measurements.java
+++ src/main/java/utility/Measurements.java
@@ -3,10 +3,10 @@ import java.util.Objects;
 
 public class Measurements {
 
-    private int quantity ;
+    private double quantity ;
     private MeasurementTypes unit;
 
-    public Measurements(int quantity, MeasurementTypes unit) {
+    public Measurements(double quantity, MeasurementTypes unit) {
         this.quantity = quantity;
         this.unit = unit;
     }
@@ -15,7 +15,16 @@ public class Measurements {
     public boolean equals(Object o) {
         if (this == o) return true;
         Measurements measurement2 = (Measurements) o;
-        return quantity == measurement2.quantity && unit == measurement2.unit;
+        if(unit == measurement2.unit){
+            return quantity == measurement2.quantity;
+        }
+        else if(unit == MeasurementTypes.METER && measurement2.unit == MeasurementTypes.CENTIMETER){
+            return quantity == (measurement2.quantity)/100 ;
+        }
+        else if(unit == MeasurementTypes.CENTIMETER && measurement2.unit == MeasurementTypes.KILOMETER){
+            return  quantity == measurement2.quantity * 100000;
+        }
+        return quantity == measurement2.quantity;
     }
 
     @Override
--- src/test/java/utility/MeasurementTest.java
+++ src/test/java/utility/MeasurementTest.java
@@ -15,4 +15,24 @@ public class MeasurementTest {
 
         assertTrue(actualValue);
     }
+
+    @Test
+    void TestIf1MeterIsEqualTo100CM(){
+        Measurements measurement1 = new Measurements(1,MeasurementTypes.METER);
+        Measurements measurement2 = new Measurements(100,MeasurementTypes.CENTIMETER);
+
+        boolean actualValue = measurement1.equals(measurement2);
+
+        assertTrue(actualValue);
+    }
+
+    @Test
+    void TestIf100CMIsEqualTo0_001KM(){
+        Measurements measurement1 = new Measurements(100,MeasurementTypes.CENTIMETER);
+        Measurements measurement2 = new Measurements(0.001,MeasurementTypes.KILOMETER);
+
+        boolean actualValue = measurement1.equals(measurement2);
+
+        assertTrue(actualValue);
+    }
 }
