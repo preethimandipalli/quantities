7da5d9dc4197d746b21c480e8fc6587cf8a54cbf
preethi <preethimandipalli99@gmail.com>
2021-04-17 22:04:57 +0530

    refactored convertion methods

--- src/main/java/utility/Temperature.java
+++ src/main/java/utility/Temperature.java
@@ -6,11 +6,9 @@ public class Temperature extends Measurements{
     }
     public double ConvertIntoDefaultUnit(double quantity, MeasurementTypes unit){
         if(unit == MeasurementTypes.CELSIUS){
-            System.out.println((quantity*1.8)+32 + ""+"ce");
             return  (quantity*1.8)+32;
         }
         else if(unit == MeasurementTypes.KELVIN){
-            System.out.println(((quantity -273.15) *1.8)+32);
             return ((quantity -273.15) *1.8)+32;
         }
         return quantity;
